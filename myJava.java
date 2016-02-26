77dc8a812321e282ac41ee21a6b47d5beccbc3d1
binyao <719921218@qq.com>
2016-02-26 14:26:02 +0800

    4th mod

--- myJava.java
+++ myJava.java
@@ -1,6 +1,6 @@
 public class myJava{
 	public static void main(String[]args){
-		System.out.println("Hi,yaobin");
+		System.out.println("Hi,yaobin,what's up");
 		for (int i=0;i<10 ;i++ ) {
 			i+=1;
 		}
